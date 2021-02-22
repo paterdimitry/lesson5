25b182ee77154f2dacf1e3e7cfae4702aa2fb80b
paterdimitry <paterdimitry@gmail.com>
2021-02-22 14:50:01 +0300

    УРОК 5. Многопоточность. Исходная задача синхронизирована

--- src/main/java/lesson5/Tunnel.java
+++ src/main/java/lesson5/Tunnel.java
@@ -1,6 +1,12 @@
 package lesson5;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
+
+    //создаем семафор с допуском половины участников в тоннель
+    Semaphore semaphore = new Semaphore(MainClass.CARS_COUNT / 2);
+
     public Tunnel() {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
@@ -11,12 +17,16 @@ public class Tunnel extends Stage {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                //участник ждет разрешения въезда в тоннель
+                semaphore.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                //после выезда из тоннеля сбрасывает семафор
+                semaphore.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
